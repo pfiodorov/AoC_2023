4ff475c4e4c04bf52ba139460a74a6ed12b8d7ee
Pavel Fiodorov <pvfiodorov@gmail.com>
2023-12-06 21:44:57 +0200

    Day 1 complete

--- day1/main.go
+++ day1/main.go
@@ -1,34 +1,77 @@
 package main
 
 import (
-    "log"
-    "bufio"
-    "fmt"
-    "os"
-    "regexp"
-    "strconv"
-    "strings"
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
 )
 
+func isInt(input string) (num int, is_int bool) {
+	number_strings := [9]string{"1", "2", "3", "4", "5", "6", "7", "8", "9"}
+	// Comment this out to pass day 1 part 1
+	written_strings := [9]string{"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"}
+
+	for index, a := range number_strings {
+		if strings.Contains(input, a) {
+			num = index + 1
+			is_int = true
+			return
+		}
+	}
+
+	for index, a := range written_strings {
+		if strings.Contains(input, a) {
+			num = index + 1
+			is_int = true
+			return
+		}
+	}
+	return 0, false
+}
+
+func findFirstInt(input string) (num string) {
+	var buf string
+	for i := 0; i < len(input); i++ {
+		buf += string(input[i])
+		num, is_int := isInt(string(buf))
+		if is_int {
+			return strconv.Itoa(num)
+		}
+	}
+	return "0"
+}
+
+func findLastInt(input string) (num string) {
+	var buf string
+	for i := len(input) - 1; i >= 0; i-- {
+		buf = string(input[i]) + buf
+		num, is_int := isInt(string(buf))
+		if is_int {
+			return strconv.Itoa(num)
+		}
+	}
+	return "0"
+}
+
 func main() {
-    file, err := os.Open("test_input2.txt")
-    if err != nil {
-        log.Fatal(err)
-    }
-    defer file.Close()
-    
-    scanner := bufio.NewScanner(file)
-    sum := 0
-    for scanner.Scan() {
-        text := scanner.Text()
-        re := regexp.MustCompile("[0-9]+")
-        clean_str_arr := re.FindAllString(text, -1)
-	clean_str := strings.Join(clean_str_arr, "")
-        res, _ := strconv.Atoi(string(clean_str[0]) + string(clean_str[len(clean_str)-1]))
-// Debug output
-//	fmt.Println(clean_str, " eq ", res)
-        sum = sum + res
-    }
-    fmt.Println("Done")
-    fmt.Println(sum)
+	file, err := os.Open("test_input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	sum := 0
+	for scanner.Scan() {
+		text := scanner.Text()
+		firstInt := findFirstInt(text)
+		lastInt := findLastInt(text)
+		res, _ := strconv.Atoi(firstInt + lastInt)
+		sum = sum + res
+	}
+	fmt.Println("Done")
+	fmt.Println(sum)
 }
